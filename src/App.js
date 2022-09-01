a398e069502b17e929590a0c1cd49024482d9a4d
samridhi14 <83155658+Shaurya2712@users.noreply.github.com>
2022-09-01 20:02:30 +0530

    still checking

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          i just made changes
+          i just made
         </a>
       </header>
     </div>
