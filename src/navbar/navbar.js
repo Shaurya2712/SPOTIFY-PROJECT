6d95216efc23f6c185ea4e2ed1fb32ab40e62d42
samridhi14 <83155658+Shaurya2712@users.noreply.github.com>
2022-09-06 09:40:30 +0530

    changes

---
+++ src/navbar/navbar.js
@@ -0,0 +1,14 @@
+import "./navbar.css";
+
+function navbar() {
+  return (
+    <>
+      <h1>hello there</h1>
+      <div>
+        <btn className="btn btn-success">search</btn>
+      </div>
+    </>
+  );
+}
+
+export default navbar;
